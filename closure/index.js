bcc22b74d737743f80fbc356a3efaf09ddfc7291
Carlos Vargas <varlopecar@gmail.com>
2022-04-13 20:11:34 -0500

    Starting closure part

---
+++ closure/index.js
@@ -0,0 +1,19 @@
+// function createFunction() {
+//   function multiplyBy2(num) {
+//     return num * 2;
+//   }
+//   return multiplyBy2;
+// }
+// const generatedFunc = createFunction();
+// const result = generatedFunc(3);
+
+// function outer() {
+//   let counter = 0;
+//   function incrementCounter() {
+//     counter++;
+//   }
+//   incrementCounter();
+//   console.log(counter);
+// }
+// outer();
+
