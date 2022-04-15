fd1547fbff41ec7aef8a8990cdf1918723c5ecfa
Carlos Vargas <varlopecar@gmail.com>
2022-04-15 12:29:36 -0500

    Studying Closure

--- closure/index.js
+++ closure/index.js
@@ -17,3 +17,14 @@
 // }
 // outer();
 
+function outer() {
+  let counter = 0;
+  function incrementCounter() {
+    counter++;
+    console.log(counter);
+  }
+  return incrementCounter;
+}
+const myNewFunction = outer();
+myNewFunction();
+myNewFunction();
